ffd5e67d234226073c900d340a7011a50020605b
sdleiw <wang@solutiondrive.de>
2017-10-14 19:41:51 +0200

    remove hello component

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Navigation from '@/components/Navigation'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/hello',
-      name: 'Hello',
-      component: Hello
-    },
     {
         path: '/',
         name: 'index',
