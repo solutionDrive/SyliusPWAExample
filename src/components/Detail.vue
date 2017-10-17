2692ec1854b18c4d651d628a8e162ec616dd0cd9
sdleiw <wang@solutiondrive.de>
2017-10-17 21:59:10 +0200

    set response, add error handling of api request

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <category></category>
-        <div>
+
+        <!--@todo: loading icon-->
+        <div v-if="loading">Loading</div>
+        <div class="container">
+            <div v-if="error" class="alert alert-danger" role="alert">{{ error }}</div>
+        </div>
+        <div v-if="product">
             <img :src="'http://demo.sylius.org/media/image/' + product.images[0].path"
                  :alt="product.name"
                  style="width: 400px;"
@@ -17,11 +23,26 @@
     export default {
         data () {
             return {
-                product: {}
+                product: {},
+                error: '',
+                loading: false
             }
         },
         created () {
-            api.getProduct(this.$route.params.code).then(response => this.product = response.data);
+            this.fetchDataFromApi()
+        },
+        watch: {
+            '$store': 'fetchDataFromApi'
+        },
+        methods: {
+            fetchDataFromApi () {
+                this.error = this.product = null
+                this.loading = true
+                api.getProduct(this.$route.params.code)
+                    .then(response => this.product = response.data)
+                    .catch(error => this.error = error.toString())
+                this.loading = false
+            }
         }
     }
 </script>
