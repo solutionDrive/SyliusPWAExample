54444322298eec118c7342b3b4936d6a8fcaa594
sdleiw <wang@solutiondrive.de>
2017-10-17 21:18:46 +0200

    add comments in the api

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,32 @@ export default {
     shop: {
         api: 'http://demo.sylius.org/shop-api'
     },
+
+    /**
+     * ======================================
+     * taxon api
+     * ======================================
+     */
+
     getAllCategories () {
         return axios.get(this.shop.api + '/taxons/')
     },
+
+    /**
+     * ======================================
+     * product api
+     * ======================================
+     */
+
     getProducts (code) {
         return axios.get(this.shop.api + '/taxon-products/' + code + '?channel=US_WEB')
     },
+    /**
+     * ======================================
+     * cart api
+     * ======================================
+     */
+
     pickUpCart (cartid) {
         const data = new FormData();
         data.append('channel', 'US_WEB');
