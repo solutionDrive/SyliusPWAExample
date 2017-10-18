eee6815eea400133a30220510c1fa39100cb6245
sdleiw <wang@solutiondrive.de>
2017-10-18 22:47:01 +0200

    rebuild list page, add todos

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,19 +2,51 @@
     <div>
         <category></category>
 
+        <div class="container">
+            @todo: breadcrumb component
+        </div>
+
+        <br>
+
         <!--@todo: loading icon-->
         <div v-if="loading">Loading</div>
         <div class="container">
-            <div v-if="error" class="alert alert-danger" role="alert">{{ error }}</div>
-        </div>
-        <div class="container" v-if="products && products.length">
-            <div class="row">
-                <div class="col-sm-3" v-for="product in products">
-                    <img style="width: 100%; height: auto;" :src="'http://demo.sylius.org/media/image/' + product.images[0].path" />
-                    <h3>{{ product.name }}</h3>
-                    <p>{{ product.slug }}</p>
-                    <router-link :to="'/detail/' + product.code">to details</router-link>
-                    <button @click="addToCart(product.code)" class="btn btn-primary">add to cart</button>
+            <div v-if="error" class="notification is-danger">{{ error }}</div>
+            <h1 class="title">@todo: category name</h1>
+            <p>@todo: category description</p>
+            <hr>
+            <div class="columns">
+                <div class="column is-one-quarter">
+                    @todo: category tree
+                </div>
+                <div class="column">
+                    <div class="columns is-multiline is-mobile">
+                        <div class="column is-full">
+                            @todo: search box
+                        </div>
+                        <div class="column is-one-third" v-for="product in products">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <router-link :to="'/detail/' + product.code">
+                                            <img :src="'http://demo.sylius.org/media/image/' + product.images[0].path" />
+                                        </router-link>
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="content">
+                                        <router-link :to="'/detail/' + product.code">{{ product.name }}</router-link>
+                                        <p>@todo first variant price</p>
+
+                                        <button @click="addToCart(product.code)" class="btn btn-primary">add to cart</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column is-full">
+                            @todo: pagination component
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
