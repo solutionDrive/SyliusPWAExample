889405013932ce4351baa0fdc276a747af5b055e
sdleiw <wang@solutiondrive.de>
2017-10-18 22:46:38 +0200

    homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        home
         <category></category>
+        <div class="container">
+            <hr>
+            <h1 class="title">Homepage</h1>
+        </div>
     </div>
 </template>
 
