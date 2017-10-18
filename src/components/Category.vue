7fc5faca2f517f5b6035a4bd51487dac9e4f4728
sdleiw <wang@solutiondrive.de>
2017-10-18 21:36:05 +0200

    implement bootstrap standart nav

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,17 +1,24 @@
 <template>
     <div class="container">
-        <!-- @todo: currently only root category and direct child category-->
-        <ul class="category--list" v-if="taxons && taxons.length">
-            <li class="list--item main" v-for="taxon in taxons">
-                <router-link :to="'/list/' + taxon.code">{{ taxon.code }}</router-link>
+        <ul class="nav nav-pills" v-if="taxons && taxons.length">
+            <li v-for="taxon in taxons"
+                class="nav-item" :class="{dropdown : taxon.hasChildren}">
 
-                <ul class="subcategory--list" v-if="taxon.children && taxon.children.length">
-                    <li class="list--item sub" v-for="child in taxon.children">
-                        <router-link :to="'/list/' + child.code"><span>{{ child.code }}</span></router-link>
-                    </li>
-                </ul>
+                <router-link :to="'/list/' + taxon.code"
+                             class="nav-link" :class="{'dropdown-toggle' : taxon.hasChildren}"
+                >
+                    <span>{{ taxon.code }}</span>
+                </router-link>
+
+                <!--@todo: show/hide dropdown-->
+                <!--<div class="dropdown-menu" v-if="taxon.hasChildren">-->
+                    <!--<div v-for="child in taxon.children">-->
+                        <!--<router-link :to="'/list/' + child.code" class="dropdown-item">{{ child.code }}</router-link>-->
+                    <!--</div>-->
+                <!--</div>-->
             </li>
         </ul>
+        <hr>
     </div>
 </template>
 
@@ -26,42 +33,18 @@
             }
         },
         created() {
-            api.getAllCategories().then( response => this.taxons = response.data[0].children)
+            api.getAllCategories().then( response => {
+                this.taxons = response.data[0].children
+                this.taxons.forEach(taxon => {
+                    taxon.hasChildren = !!(taxon.children && taxon.children.length)
+                })
+            })
         }
     }
 </script>
 
 <style>
-    .category--list {
-        width: 100%;
-        padding: 10px;
-        overflow: hidden;
-        box-sizing: border-box;
-    }
-    .category--list .list--item {
-        width: 25%;
-        color: #fff;
-        padding: 0 10px;
-        text-align: left;
-        background: #2c3e50;
-        box-sizing: border-box;
-    }
-
-    .category--list .list--item a {
-        text-decoration: none;
-        color: #fff;
-    }
-
-    .category--list .list--item.main {
-        float: left;
-        overflow: hidden;
-    }
-
-    .category--list .list--item.sub {
-
-    }
-
-    .subcategory--list {
-        list-style: none;
+    .nav.nav-pills {
+        background: #eee;
     }
 </style>
