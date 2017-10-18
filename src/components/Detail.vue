6b4228d73b25ec6f9ff97375a54cb03b63600c41
sdleiw <wang@solutiondrive.de>
2017-10-18 23:33:48 +0200

    rebuild details page with bulma + addtocart button

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -2,23 +2,81 @@
     <div>
         <category></category>
 
+        <br>
+
         <!--@todo: loading icon-->
         <div v-if="loading">Loading</div>
         <div class="container">
-            <div v-if="error" class="alert alert-danger" role="alert">{{ error }}</div>
-        </div>
-        <div v-if="product">
-            <img :src="'http://demo.sylius.org/media/image/' + product.images[0].path"
-                 :alt="product.name"
-                 style="width: 400px;"
-            >
-            <p>{{product.name}}</p>
+            <div v-if="error" class="notification is-danger">{{ error }}</div>
+
+            <div v-if="product">
+                <div class="columns">
+                    <div class="column is-half-tablet">
+                        <img :src="'http://demo.sylius.org/media/image/' + product.images[0].path"
+                             :alt="product.name"
+                        >
+                        <hr>
+                        <div class="columns is-multiline">
+                            <div class="column is-one-third" v-for="thumbnail in product.images">
+                                <img :src="'http://demo.sylius.org/media/image/' + thumbnail.path"
+                                     :alt="product.name"
+                                >
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-half-tablet">
+                        <h1 class="title">{{product.name}}</h1>
+                        <hr>
+                        <div>@todo: review</div>
+                        <div class="columns">
+                            <div class="column is-half">
+                                @todo: price first variant
+                            </div>
+                            <div class="column is-half">
+                                <small>{{product.code}}</small>
+                            </div>
+                        </div>
+                        <p>@todo: product description</p>
+                        <div class="box">
+                            @todo: product variant
+                            <div>
+                                <a @click="addToCart(product.code)" class="button is-link">
+                                    add first variant to cart
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tabs is-boxed">
+                    <ul>
+                        <li class="is-active">
+                            <a>
+                                <span>Details</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <span>Attributes</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <span>Reviews</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <p>
+                    @todo: similar product
+                </p>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import api from './../api'
+    import {mapState} from 'vuex'
 
     export default {
         data () {
@@ -28,6 +86,9 @@
                 loading: false
             }
         },
+        computed: mapState([
+            'cartid'
+        ]),
         created () {
             this.fetchDataFromApi()
         },
@@ -42,6 +103,14 @@
                     .then(response => this.product = response.data)
                     .catch(error => this.error = error.toString())
                 this.loading = false
+            },
+            addToCart(productCode) {
+                if (this.cartid === '') {
+                    this.$store.commit('initCartId')
+                    api.pickUpCart(this.cartid).then(response => api.addToCart(productCode, this.cartid))
+                } else {
+                    api.addToCart(productCode, this.cartid)
+                }
             }
         }
     }
