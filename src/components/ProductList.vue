3bc11ee8253a5f487a4de17e6f5b1f6158c164de
sdleiw <wang@solutiondrive.de>
2017-10-18 23:32:07 +0200

    remove add to cart button from list page

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -37,8 +37,6 @@
                                     <div class="content">
                                         <router-link :to="'/detail/' + product.code">{{ product.name }}</router-link>
                                         <p>@todo first variant price</p>
-
-                                        <button @click="addToCart(product.code)" class="btn btn-primary">add to cart</button>
                                     </div>
                                 </div>
                             </div>
@@ -67,7 +65,6 @@
             }
         },
         computed: mapState([
-            'cartid',
             'products'
         ]),
         created() {
@@ -84,17 +81,6 @@
                     .then(response => this.$store.commit('setProducts',response.data.items))
                     .catch(error => this.error = error.toString())
                 this.loading = false
-            },
-            addToCart(productCode) {
-                if (this.cartid === '') {
-                    this.$store.commit('initCartId')
-                    api.pickUpCart(this.cartid).then(response => api.addToCart(productCode, this.cartid))
-                } else {
-                    api.addToCart(productCode, this.cartid)
-                }
-            },
-            fetchCart() {
-                api.getCart(this.cartid).then(response => this.cart = response);
             }
         }
     }
