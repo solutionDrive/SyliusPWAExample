1342dccab46b38d42b1eac2a1ebea5cff4358871
sdleiw <wang@solutiondrive.de>
2017-10-19 23:19:09 +0200

    add component minicart

---
+++ src/components/MiniCart.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <span class="tag is-light is-large">cart</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'miniCart'
+    }
+</script>
