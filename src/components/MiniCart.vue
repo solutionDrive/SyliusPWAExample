d314eddfe3347e7fd1999df3178ab5e2830d06b8
sdleiw <wang@solutiondrive.de>
2017-10-19 23:58:26 +0200

    style mini cart, add init value

--- src/components/MiniCart.vue
+++ src/components/MiniCart.vue
@@ -1,11 +1,31 @@
 <template>
     <div>
-        <span class="tag is-dark is-large is-rounded">cart</span>
+        <nav class="level">
+            <div class="level-left"></div>
+            <div class="level-right">
+                <p class="level-item">
+                    <a class="button is-light">
+                        <span>{{ total + currency}}</span>
+                    </a>
+                </p>
+            </div>
+        </nav>
     </div>
 </template>
 
 <script>
+    import {mapState} from 'vuex'
+
     export default {
-        name: 'miniCart'
+        name: 'miniCart',
+        data: function () {
+            return {
+                total: '0.00',
+                currency: 'USD'
+            }
+        },
+        computed: mapState([
+            'cartid'
+        ])
     }
 </script>
