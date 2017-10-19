26fb80ec2c7b163c452bbf8803e7e09694c23024
sdleiw <wang@solutiondrive.de>
2017-10-19 20:50:44 +0200

    add section wrapper for container

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -6,69 +6,71 @@
 
         <!--@todo: loading icon-->
         <div v-if="loading">Loading</div>
-        <div class="container">
-            <div v-if="error" class="notification is-danger">{{ error }}</div>
+        <div class="section">
+            <div class="container">
+                <div v-if="error" class="notification is-danger">{{ error }}</div>
 
-            <div v-if="product">
-                <div class="columns">
-                    <div class="column is-half-tablet">
-                        <img :src="'http://demo.sylius.org/media/image/' + product.images[0].path"
-                             :alt="product.name"
-                        >
-                        <hr>
-                        <div class="columns is-multiline">
-                            <div class="column is-one-third" v-for="thumbnail in product.images">
-                                <img :src="'http://demo.sylius.org/media/image/' + thumbnail.path"
-                                     :alt="product.name"
-                                >
+                <div v-if="product">
+                    <div class="columns">
+                        <div class="column is-half-tablet">
+                            <img :src="'http://demo.sylius.org/media/image/' + product.images[0].path"
+                                 :alt="product.name"
+                            >
+                            <hr>
+                            <div class="columns is-mobile">
+                                <div class="column is-one-third" v-for="thumbnail in product.images">
+                                    <img :src="'http://demo.sylius.org/media/image/' + thumbnail.path"
+                                         :alt="product.name"
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column is-half-tablet">
+                            <h1 class="title">{{product.name}}</h1>
+                            <hr>
+                            <div>@todo: review</div>
+                            <div class="columns">
+                                <div class="column is-half">
+                                    @todo: price first variant
+                                </div>
+                                <div class="column is-half">
+                                    <small>{{product.code}}</small>
+                                </div>
+                            </div>
+                            <p>@todo: product description</p>
+                            <div class="box">
+                                @todo: product variant
+                                <div>
+                                    <a @click="addToCart(product.code)" class="button is-link">
+                                        add first variant to cart
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="column is-half-tablet">
-                        <h1 class="title">{{product.name}}</h1>
-                        <hr>
-                        <div>@todo: review</div>
-                        <div class="columns">
-                            <div class="column is-half">
-                                @todo: price first variant
-                            </div>
-                            <div class="column is-half">
-                                <small>{{product.code}}</small>
-                            </div>
-                        </div>
-                        <p>@todo: product description</p>
-                        <div class="box">
-                            @todo: product variant
-                            <div>
-                                <a @click="addToCart(product.code)" class="button is-link">
-                                    add first variant to cart
+                    <div class="tabs is-boxed">
+                        <ul>
+                            <li class="is-active">
+                                <a>
+                                    <span>Details</span>
                                 </a>
-                            </div>
-                        </div>
+                            </li>
+                            <li>
+                                <a>
+                                    <span>Attributes</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    <span>Reviews</span>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
+                    <p>
+                        @todo: similar product
+                    </p>
                 </div>
-                <div class="tabs is-boxed">
-                    <ul>
-                        <li class="is-active">
-                            <a>
-                                <span>Details</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <span>Attributes</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a>
-                                <span>Reviews</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <p>
-                    @todo: similar product
-                </p>
             </div>
         </div>
     </div>
