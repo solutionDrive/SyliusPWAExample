0af4b8081ea0f4b849dc5abc102755f15bfe94c0
sdleiw <wang@solutiondrive.de>
2017-10-19 21:15:29 +0200

    add imageUrl and apiUrl in the global config

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
 export default {
-    imageUrl: 'http://demo.sylius.org/media/image/'
+    imageUrl: 'http://127.0.0.1:8000/media/image/',
+    apiUrl: 'http://127.0.0.1:8000/shop-api',
+    apiChannel: 'US_WEB'
 }
