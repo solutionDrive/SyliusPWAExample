a5b5e36ae13067a69b4501870d522d450de04dc5
sdleiw <wang@solutiondrive.de>
2017-10-22 14:24:37 +0200

    move cart store to cart module

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import uuidRandom from 'uuid-random'
 import cart from './modules/cart'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        cartid: '',
         products: []
     },
     mutations: {
-        initCartId (state) {
-            state.cartid = uuidRandom();
-        },
-        setCartId (state, id) {
-            state.cartid = id
-        },
         setProducts (state, products) {
             state.products = products
         }
--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,7 +1,24 @@
+import uuidRandom from 'uuid-random'
+
 const state = {
+    cartid: '',
     cart: {}
 }
 
+const mutations = {
+    initCartId (state) {
+        state.cartid = uuidRandom();
+    },
+    setCartId (state, id) {
+        state.cartid = id
+    },
+    setCart (state, cart) {
+        state.cart = cart
+    }
+}
+
 export default {
-    state
+    namespaced: true,
+    state,
+    mutations
 }
