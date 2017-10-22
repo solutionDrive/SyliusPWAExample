7ce244fcea0a2034b020c8c4590cafbde3d807e0
sdleiw <wang@solutiondrive.de>
2017-10-22 21:17:29 +0200

    add category state and setter mutation

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        products: []
+        products: [],
+        category: {}
     },
     mutations: {
         setProducts (state, products) {
@@ -14,6 +15,9 @@ export default new Vuex.Store({
         },
         reset (state) {
             state.products = []
+        },
+        setCategory (state, category) {
+            state.category = category
         }
     },
     modules: {
