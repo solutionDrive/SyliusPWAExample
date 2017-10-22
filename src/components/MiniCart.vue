083542699adb2232227ff7d004ef8d1943702c7c
sdleiw <wang@solutiondrive.de>
2017-10-22 14:25:18 +0200

    minicart, add item, change cart properties to computed instead of data

--- src/components/MiniCart.vue
+++ src/components/MiniCart.vue
@@ -6,6 +6,9 @@
                 <p class="level-item">
                     <router-link to="/cart" class="button is-light">
                         <span>{{ total + currency}}</span>
+                        <span v-if="itemCount">
+                            , {{ itemCount }} item
+                        </span>
                     </router-link>
                 </p>
             </div>
@@ -15,17 +18,32 @@
 
 <script>
     import {mapState} from 'vuex'
+    import api from '@/api'
 
     export default {
         name: 'miniCart',
-        data: function () {
-            return {
-                total: '0.00',
-                currency: 'USD'
-            }
+        computed: {
+            total () {
+                return this.empty(this.cart) ? '0.00' : this.cart.totals.items / 100
+            },
+            currency () {
+                return this.empty(this.cart) ? 'USD' : this.cart.currency
+            },
+            itemCount () {
+                return this.empty(this.cart) ? 0 : this.cart.items.length
+            },
+            ...mapState({
+                cart: state => state.cart.cart,
+//                cartid: state => state.cart.cartid
+            })
         },
-        computed: mapState([
-            'cartid'
-        ])
+//        created () {
+//            api.getCart(this.cartid).then(response => this.$store.commit('cart/setCart', response.data))
+//        },
+        methods: {
+            empty () {
+                return Object.keys(this.cart).length == 0
+            }
+        }
     }
 </script>
