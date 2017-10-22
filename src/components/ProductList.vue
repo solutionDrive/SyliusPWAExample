85e5fac44b1ec0c7c8cc5d0aa4eabb1dbf7d49eb
sdleiw <wang@solutiondrive.de>
2017-10-22 18:29:39 +0200

    add breadcrumb and refactoring list page

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,44 +1,52 @@
 <template>
-    <div class="section">
+    <div>
         <div class="container">
-            @todo: breadcrumb component
+            <nav class="breadcrumb" aria-label="breadcrumbs">
+                <ul>
+                    <li><a href="#">{{ appName }}</a></li>
+                    <li><a href="#">Documentation</a></li>
+                    <li><a href="#">Components</a></li>
+                    <li class="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
+                </ul>
+            </nav>
         </div>
 
-        <br>
-        <div class="container">
-            <div v-if="error" class="notification is-danger">{{ error }}</div>
-            <h1 class="title">@todo: category name</h1>
-            <p>@todo: category description</p>
-            <hr>
-            <div class="section" v-if="loading"><clip-loader></clip-loader></div>
-            <div class="columns">
-                <div class="column is-one-quarter">
-                    @todo: category tree
-                </div>
-                <div class="column">
-                    <div class="columns is-multiline is-mobile">
-                        <div class="column is-full">
-                            @todo: search box
-                        </div>
-                        <div class="column is-one-third" v-for="product in products">
-                            <div class="card">
-                                <div class="card-image">
-                                    <figure class="image is-4by3">
-                                        <router-link :to="'/detail/' + product.code">
-                                            <img :src="imageUrl + product.images[0].path" />
-                                        </router-link>
-                                    </figure>
-                                </div>
-                                <div class="card-content">
-                                    <div class="content">
-                                        <router-link :to="'/detail/' + product.code">{{ product.name }}</router-link>
-                                        <p>@todo first variant price</p>
+        <div class="section">
+            <div class="container">
+                <div v-if="error" class="notification is-danger">{{ error }}</div>
+                <h1 class="title">@todo: category name</h1>
+                <p>@todo: category description</p>
+                <hr>
+                <div class="section" v-if="loading"><clip-loader></clip-loader></div>
+                <div class="columns">
+                    <div class="column is-one-quarter">
+                        @todo: category tree
+                    </div>
+                    <div class="column">
+                        <div class="columns is-multiline is-mobile">
+                            <div class="column is-full">
+                                @todo: search box
+                            </div>
+                            <div class="column is-one-third" v-for="product in products">
+                                <div class="card">
+                                    <div class="card-image">
+                                        <figure class="image is-4by3">
+                                            <router-link :to="'/detail/' + product.code">
+                                                <img :src="imageUrl + product.images[0].path" />
+                                            </router-link>
+                                        </figure>
+                                    </div>
+                                    <div class="card-content">
+                                        <div class="content">
+                                            <router-link :to="'/detail/' + product.code">{{ product.name }}</router-link>
+                                            <p>@todo first variant price</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="column is-full">
-                            @todo: pagination component
+                            <div class="column is-full">
+                                @todo: pagination component
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -59,7 +67,7 @@
                 cart:{},
                 loading: false,
                 error: '',
-                imageUrl: '',
+                imageUrl: appConfig.imageUrl,
                 appName: appConfig.appName
             }
         },
@@ -68,7 +76,6 @@
         ]),
         created() {
             this.fetchDataFromApi()
-            this.imageUrl = appConfig.imageUrl
         },
         watch: {
             '$route': 'fetchDataFromApi'
