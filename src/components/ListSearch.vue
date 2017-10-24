88fd892c40376c256a6e19783bae4b8ada3c90a2
sdleiw <wang@solutiondrive.de>
2017-10-24 23:14:22 +0200

    add ListSearch Component

--- src/components/ListSearch.vue
+++ src/components/ListSearch.vue
@@ -2,7 +2,8 @@
     <div class="box">
         <div class="field has-addons">
             <div class="control is-expanded">
-                <input class="input" type="text" placeholder="Find a product on this page">
+                <input v-model="query"
+                       class="input" type="text" placeholder="Find a product on this page">
             </div>
             <div class="control">
                 <a class="button is-link">
@@ -10,16 +11,25 @@
                 </a>
             </div>
             <div class="control">
-                <a class="button is-warning">
+                <a class="button is-warning" >
                     Clear
                 </a>
             </div>
+        </div>
+        <div>
+            {{query}}
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'list-search'
+        // @todo: still not implemented, need a search api + query parameter
+        name: 'list-search',
+        data () {
+            return {
+                query: ''
+            }
+        }
     }
 </script>
