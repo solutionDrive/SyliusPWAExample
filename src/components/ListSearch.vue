b3776290c4af0b21f4934af1709447303c907853
sdleiw <wang@solutiondrive.de>
2017-10-24 22:24:05 +0200

    styling search box

--- src/components/ListSearch.vue
+++ src/components/ListSearch.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
+    <div class="box">
         <div class="field has-addons">
-            <div class="control">
-                <input class="input" type="text" placeholder="Find a repository">
+            <div class="control is-expanded">
+                <input class="input" type="text" placeholder="Find a product on this page">
             </div>
             <div class="control">
-                <a class="button is-info">
+                <a class="button is-link">
                     Search
+                </a>
+            </div>
+            <div class="control">
+                <a class="button is-warning">
+                    Clear
                 </a>
             </div>
         </div>
