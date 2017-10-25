c5f02403d553235ca14fb9c508394dc9d1fc70f7
sdleiw <wang@solutiondrive.de>
2017-10-25 23:22:45 +0200

    addtoCart function on variant

--- src/components/DetailVariant.vue
+++ src/components/DetailVariant.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="box">
         <div class="field">
+            <label class="label">Variant</label>
+            <div class="control">
+                <div class="select is-fullwidth" :class="{ 'is-danger': error }">
+                    <select v-model="variantCode" v-on:change="resetError">
+                        <option v-for="variant in product.variants"
+                                :value="variant.code">
+                            {{getVariantName(variant)}}
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <p class="help is-danger" v-if="error">{{error}}</p>
+        </div>
+        <div class="field">
             <label class="label">Quantity</label>
             <div class="control">
                 <input v-model="quantity" class="input" type="number" min="1">
             </div>
         </div>
         <div class="control">
-            <button @click="addToCart(product.code)" class="button is-link">add first variant to cart</button>
+            <button @click="addToCart()" class="button is-link">add first variant to cart</button>
         </div>
     </div>
 </template>
@@ -22,7 +36,8 @@
             return {
                 loading: false,
                 error: '',
-                quantity: 1
+                quantity: 1,
+                variantCode: ''
             }
         },
         props: [
@@ -34,18 +49,29 @@
             })
         },
         methods: {
-            async addToCart(productCode) {
+            getVariantName (variant) {
+                return Object.values(variant.nameAxis).join()
+            },
+            resetError () {
+                this.error = ''
+            },
+            async addToCart() {
+                if (!this.variantCode) {
+                    this.error = 'choose a variant'
+                    return
+                }
+
                 if (this.cartid === '') {
                     this.$store.commit('cart/initCartId')
                     await cartApi.pickUpCart(this.cartid);
                 }
 
-                await this.updateAfterAddToCart(productCode)
+                await this.updateAfterAddToCart()
             },
-            async updateAfterAddToCart (productCode) {
+            async updateAfterAddToCart () {
                 try {
                     this.loading = true
-                    await cartApi.addToCart(productCode, this.cartid)
+                    await cartApi.addToCart(this.cartid, this.product.code, this.quantity, this.variantCode)
                     let cart = await cartApi.getCart(this.cartid)
                     this.$store.commit('cart/setCart', cart.data)
                 } catch (error) {
