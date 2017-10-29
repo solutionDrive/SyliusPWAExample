190e4d0622181caac5cc10c62ea10e28d5a1f2e1
sdleiw <wang@solutiondrive.de>
2017-10-29 23:40:41 +0100

    add unit test for store detail module

---
+++ test/unit/specs/store/modules/details.spec.js
@@ -0,0 +1,24 @@
+import detail from '@/store/modules/detail'
+
+const {setProduct, resetProduct} = detail.mutations
+
+describe('detail store module', () => {
+    it('should set product state', () => {
+        const state = {product: {}}
+        const mockProduct = {name: 'mock'}
+
+        setProduct(state, mockProduct)
+
+        expect(state.product).to.equal(mockProduct)
+    })
+
+    it('should reset product state', () => {
+        const state = {product: {}}
+        const mockProduct = {name: 'mock'}
+        setProduct(state, mockProduct)
+
+        resetProduct(state)
+
+        expect(state.product).to.be.an('object').that.is.empty
+    })
+})
