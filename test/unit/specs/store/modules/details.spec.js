5f27bb0936e36912d5930c0b64d0a3c77ecce036
sdleiw <wang@solutiondrive.de>
2017-10-30 00:17:48 +0100

    rename mock to test

--- test/unit/specs/store/modules/details.spec.js
+++ test/unit/specs/store/modules/details.spec.js
@@ -3,18 +3,18 @@ import detail from '@/store/modules/detail'
 const {setProduct, resetProduct} = detail.mutations
 
 describe('detail store module', () => {
-    const mockProduct = {name: 'mock'}
+    const testProduct = {name: 'test'}
 
     it('should set product state', () => {
         const state = {product: {}}
-        setProduct(state, mockProduct)
+        setProduct(state, testProduct)
 
-        expect(state.product).to.equal(mockProduct)
+        expect(state.product).to.equal(testProduct)
     })
 
     it('should reset product state', () => {
         const state = {product: {}}
-        setProduct(state, mockProduct)
+        setProduct(state, testProduct)
         resetProduct(state)
 
         expect(state.product).to.be.an('object').that.is.empty
--- test/unit/specs/store/modules/list.spec.js
+++ test/unit/specs/store/modules/list.spec.js
@@ -1,19 +1,19 @@
 import list from '@/store/modules/list'
 
 describe('list store module', () => {
-    const mockProducts = ['product1', 'product2']
-    const mockCategory = {name: 'mock'}
+    const testProducts = ['product1', 'product2']
+    const testCategory = {name: 'test'}
 
     it('should set products on the list', () => {
         const state = {products: []}
-        list.mutations.setProducts(state, mockProducts)
+        list.mutations.setProducts(state, testProducts)
 
-        expect(state.products).to.equal(mockProducts)
+        expect(state.products).to.equal(testProducts)
     })
 
     it('should reset products on the list', () => {
         const state = {products: []}
-        list.mutations.setProducts(state, mockProducts)
+        list.mutations.setProducts(state, testProducts)
         list.mutations.resetProducts(state)
 
         expect(state.products).to.be.an('array').that.is.empty
@@ -21,14 +21,14 @@ describe('list store module', () => {
 
     it('should set current category', () => {
         const state = {category: {}}
-        list.mutations.setCategory(state, mockCategory)
+        list.mutations.setCategory(state, testCategory)
 
-        expect(state.category).to.equal(mockCategory)
+        expect(state.category).to.equal(testCategory)
     })
 
     it('should reset current category', () => {
         const state = {category: {}}
-        list.mutations.setCategory(state, mockCategory)
+        list.mutations.setCategory(state, testCategory)
         list.mutations.resetCategory(state)
 
         expect(state.category).to.be.an('object').that.is.empty
