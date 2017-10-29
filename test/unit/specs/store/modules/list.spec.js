ac4fc40f82df855224b4de2264564a8a22b53e25
sdleiw <wang@solutiondrive.de>
2017-10-30 00:03:59 +0100

    add tests for category setter, reset and product reset

--- test/unit/specs/store/modules/list.spec.js
+++ test/unit/specs/store/modules/list.spec.js
@@ -1,11 +1,36 @@
 import list from '@/store/modules/list'
 
 describe('list store module', () => {
-    it('should set products', () => {
+    const mockProducts = ['product1', 'product2']
+    const mockCategory = {name: 'mock'}
+
+    it('should set products on the list', () => {
         const state = {products: []}
-        const mockProducts = ['product1', 'product2']
         list.mutations.setProducts(state, mockProducts)
 
         expect(state.products).to.equal(mockProducts)
+    })
+
+    it('should reset products on the list', () => {
+        const state = {products: []}
+        list.mutations.setProducts(state, mockProducts)
+        list.mutations.resetProducts(state)
+
+        expect(state.products).to.be.an('array').that.is.empty
+    })
+
+    it('should set current category', () => {
+        const state = {category: {}}
+        list.mutations.setCategory(state, mockCategory)
+
+        expect(state.category).to.equal(mockCategory)
+    })
+
+    it('should reset current category', () => {
+        const state = {category: {}}
+        list.mutations.setCategory(state, mockCategory)
+        list.mutations.resetCategory(state)
+
+        expect(state.category).to.be.an('object').that.is.empty
     })
 })
