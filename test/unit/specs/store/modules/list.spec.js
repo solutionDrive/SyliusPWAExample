93a34ca95f500a288696d0636d5cec455b0b641b
sdleiw <wang@solutiondrive.de>
2017-10-30 00:19:26 +0100

    deconstruct functions from list mutations

--- test/unit/specs/store/modules/list.spec.js
+++ test/unit/specs/store/modules/list.spec.js
@@ -1,35 +1,37 @@
 import list from '@/store/modules/list'
 
+const {setProducts, resetProducts, setCategory, resetCategory} = list.mutations
+
 describe('list store module', () => {
     const testProducts = ['product1', 'product2']
     const testCategory = {name: 'test'}
 
     it('should set products on the list', () => {
         const state = {products: []}
-        list.mutations.setProducts(state, testProducts)
+        setProducts(state, testProducts)
 
         expect(state.products).to.equal(testProducts)
     })
 
     it('should reset products on the list', () => {
         const state = {products: []}
-        list.mutations.setProducts(state, testProducts)
-        list.mutations.resetProducts(state)
+        setProducts(state, testProducts)
+        resetProducts(state)
 
         expect(state.products).to.be.an('array').that.is.empty
     })
 
     it('should set current category', () => {
         const state = {category: {}}
-        list.mutations.setCategory(state, testCategory)
+        setCategory(state, testCategory)
 
         expect(state.category).to.equal(testCategory)
     })
 
     it('should reset current category', () => {
         const state = {category: {}}
-        list.mutations.setCategory(state, testCategory)
-        list.mutations.resetCategory(state)
+        setCategory(state, testCategory)
+        resetCategory(state)
 
         expect(state.category).to.be.an('object').that.is.empty
     })
