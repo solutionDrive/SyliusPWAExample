f46c76db50114d993b2e72da998854b5484ef61e
sdleiw <wang@solutiondrive.de>
2017-10-31 00:13:40 +0100

    indentatoin

--- src/components/CartDashboard.vue
+++ src/components/CartDashboard.vue
@@ -11,32 +11,32 @@
                 </tr>
             </thead>
             <tbody>
-            <tr v-for="item in cart.items">
-                <td>
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image is-64x64">
-                                <img :src="imageUrl + item.product.images[0].path"
-                                     :alt="item.product.name"
-                                >
-                            </figure>
-                        </div>
-                        <div class="media-content">
-                            <div class="content">
-                                <router-link :to="'/detail/' + item.product.code">{{ item.product.name }}</router-link>
-                                <p><small>{{item.product.code}}</small></p>
-                                <p><small>{{item.product.variants[0].axis[0]}}</small></p>
+                <tr v-for="item in cart.items">
+                    <td>
+                        <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img :src="imageUrl + item.product.images[0].path"
+                                         :alt="item.product.name"
+                                    >
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <router-link :to="'/detail/' + item.product.code">{{ item.product.name }}</router-link>
+                                    <p><small>{{item.product.code}}</small></p>
+                                    <p><small>{{item.product.variants[0].axis[0]}}</small></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </td>
-                <td>
-                    {{item.product.variants[0].price.current / 100 + cart.currency}}
-                </td>
-                <td>{{item.quantity}}</td>
-                <td><a class="delete"></a></td>
-                <td>{{item.total / 100 + cart.currency}}</td>
-            </tr>
+                    </td>
+                    <td>
+                        {{item.product.variants[0].price.current / 100 + cart.currency}}
+                    </td>
+                    <td>{{item.quantity}}</td>
+                    <td><a class="delete"></a></td>
+                    <td>{{item.total / 100 + cart.currency}}</td>
+                </tr>
             </tbody>
         </div>
         <div>
