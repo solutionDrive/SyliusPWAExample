48cf911140e20639391e389882b935b1e18eed78
sdleiw <wang@solutiondrive.de>
2017-10-31 00:18:05 +0100

    remove unneccessary global component

--- src/main.js
+++ src/main.js
@@ -4,18 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import List from './components/List'
-import Cart from './components/Cart'
-import Detail from './components/Detail'
-import Home from './components/Home'
 
 Vue.config.productionTip = false
 
 Vue.component('app', App)
-Vue.component('list', List)
-Vue.component('cart', Cart)
-Vue.component('detail', Detail)
-Vue.component('home', Home)
 
 new Vue({
     el: '#app',
