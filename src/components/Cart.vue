537903be4faf29cffe674a3faeb190f9b4453a86
sdleiw <wang@solutiondrive.de>
2017-10-31 00:09:07 +0100

    add cartDashboard Component

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -15,53 +15,7 @@
 
                 <div class="columns">
                     <div class="column is-two-thirds">
-                        <div class="box">
-                            <div class="table">
-                                <thead>
-                                    <tr>
-                                        <th>item</th>
-                                        <th><abbr title="unit price">unit price</abbr></th>
-                                        <th><abbr title="quantity">Qty</abbr></th>
-                                        <th>&nbsp;</th>
-                                        <th>total</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr v-for="item in cart.items">
-                                        <td>
-                                            <div class="media">
-                                                <div class="media-left">
-                                                    <figure class="image is-64x64">
-                                                        <img :src="imageUrl + item.product.images[0].path"
-                                                             :alt="item.product.name"
-                                                        >
-                                                    </figure>
-                                                </div>
-                                                <div class="media-content">
-                                                    <div class="content">
-                                                        <router-link :to="'/detail/' + item.product.code">{{ item.product.name }}</router-link>
-                                                        <p><small>{{item.product.code}}</small></p>
-                                                        <p><small>{{item.product.variants[0].axis[0]}}</small></p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            {{item.product.variants[0].price.current / 100 + cart.currency}}
-                                        </td>
-                                        <td>{{item.quantity}}</td>
-                                        <td><a class="delete"></a></td>
-                                        <td>{{item.total / 100 + cart.currency}}</td>
-                                    </tr>
-                                </tbody>
-                            </div>
-                            <div>
-                                @todo: apply coupon
-                            </div>
-                            <div>
-                                @todo: update cart button
-                            </div>
-                        </div>
+                        <cart-dashboard :cart = cart></cart-dashboard>
                     </div>
                     <div class="column is-one-third">
                         <cart-summary :cart = cart></cart-summary>
@@ -77,17 +31,16 @@
 
 <script>
     import {cartApi} from '@/api'
-    import appConfig from '@/config'
     import {mapState} from 'vuex'
     import ClipLoader from 'vue-spinner/src/ClipLoader'
     import CartSummary from '@/components/CartSummary'
+    import CartDashboard from '@/components/CartDashboard'
 
     export default {
         data () {
             return {
                 loading: false,
-                emptyCart: false,
-                imageUrl: appConfig.imageUrl
+                emptyCart: false
             }
         },
         computed: {
@@ -114,7 +67,8 @@
         },
         components: {
             ClipLoader,
-            CartSummary
+            CartSummary,
+            CartDashboard
         }
     }
 </script>
---
+++ src/components/CartDashboard.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="box" v-if="!isEmpty(cart)">
+        <div class="table">
+            <thead>
+                <tr>
+                    <th>item</th>
+                    <th><abbr title="unit price">unit price</abbr></th>
+                    <th><abbr title="quantity">Qty</abbr></th>
+                    <th>&nbsp;</th>
+                    <th>total</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in cart.items">
+                <td>
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <img :src="imageUrl + item.product.images[0].path"
+                                     :alt="item.product.name"
+                                >
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content">
+                                <router-link :to="'/detail/' + item.product.code">{{ item.product.name }}</router-link>
+                                <p><small>{{item.product.code}}</small></p>
+                                <p><small>{{item.product.variants[0].axis[0]}}</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    {{item.product.variants[0].price.current / 100 + cart.currency}}
+                </td>
+                <td>{{item.quantity}}</td>
+                <td><a class="delete"></a></td>
+                <td>{{item.total / 100 + cart.currency}}</td>
+            </tr>
+            </tbody>
+        </div>
+        <div>
+            @todo: apply coupon
+        </div>
+        <div>
+            @todo: update cart button
+        </div>
+    </div>
+</template>
+
+<script>
+    import {isEmpty} from 'lodash'
+    import appConfig from '@/config'
+
+    export default {
+        name: 'cart-dashboard',
+        data () {
+            return {
+                imageUrl: appConfig.imageUrl
+            }
+        },
+        props: [
+            'cart'
+        ],
+        methods: {
+            isEmpty
+        }
+    }
+</script>
