5c637b0d9e21d8f33358a9c3b4f20e717cfe4f17
sdleiw <wang@solutiondrive.de>
2017-10-31 17:02:59 +0100

    add product card + product object

--- src/views/pages/detail/review/DetailReview.vue
+++ src/views/pages/detail/review/DetailReview.vue
@@ -1,16 +1,59 @@
 <template>
     <div class="section">
         <div class="container">
-            review
+            <div v-if="error" class="notification is-danger">{{ error }}</div>
+
+            <div class="columns is-multiline">
+                <div class="column is-one-quarter" v-if="!isEmpty(product)">
+                    <product-card :product = product></product-card>
+                </div>
+                <div class="column is-three-quarters">
+                    <article class="media box">
+                        <div class="media-content">
+                            <div class="content">
+                                <p>
+                                    <strong>John Smith</strong>
+                                    <small>@johnsmith</small>
+                                    <small>31m</small>
+                                    <br>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import {isEmpty} from 'lodash'
+    import {productApi} from '@/api'
     import ProductCard from '@/views/components/ProductCard'
 
     export default {
         name: 'detail-review',
+        data () {
+            return {
+                error: '',
+                product: {}
+            }
+        },
+        created () {
+            this.fetchProduct()
+        },
+        methods: {
+            async fetchProduct () {
+                try {
+                    const product = await productApi.getProductBySlug(this.$route.params.slug)
+                    this.product = product.data
+                } catch (error) {
+                    this.error = error.toString()
+                }
+            },
+            isEmpty
+        },
         components: {
             ProductCard
         }
