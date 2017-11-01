c9a679b74a9cd390f03417d6faab2bc379cc402a
sdleiw <wang@solutiondrive.de>
2017-11-01 17:07:19 +0100

    optimize detail page, remove isEmpty

--- src/views/pages/detail/Detail.vue
+++ src/views/pages/detail/Detail.vue
@@ -3,38 +3,34 @@
         <breadcrumb></breadcrumb>
 
         <div class="section">
-            <div class="container">
-                <div class="section" v-if="loading"><clip-loader></clip-loader></div>
+            <div class="section" v-if="loading"><clip-loader></clip-loader></div>
+            <div class="container" v-if="!loading">
                 <div v-if="error" class="notification is-danger">{{ error }}</div>
-
-                <div v-if="!isEmpty(product)">
-                    <div class="columns">
-                        <div class="column is-half-tablet">
-                            <detail-product-image :product=product></detail-product-image>
-                        </div>
-                        <div class="column is-half-tablet">
-                            <h1 class="title">{{product.name}}</h1>
-                            <hr>
-                            <detail-rating-link :rating = product.averageRating
-                                                :count = review.length>
-                            </detail-rating-link>
-                            <detail-variant :product = product></detail-variant>
-                        </div>
+                <div class="columns">
+                    <div class="column is-half-tablet">
+                        <detail-product-image :product=product></detail-product-image>
                     </div>
-                    <detail-product-tab :review = review></detail-product-tab>
-                    <hr>
-                    <div class="has-text-centered latest-product-header">
-                        <strong>Latest Products</strong>
+                    <div class="column is-half-tablet">
+                        <h1 class="title">{{product.name}}</h1>
+                        <hr>
+                        <detail-rating-link :rating = product.averageRating
+                                            :count = review.length>
+                        </detail-rating-link>
+                        <detail-variant :product = product></detail-variant>
                     </div>
-                    <latest-products></latest-products>
                 </div>
+                <detail-product-tab :review = review></detail-product-tab>
+                <hr>
+                <div class="has-text-centered latest-product-header">
+                    <strong>Latest Products</strong>
+                </div>
+                <latest-products></latest-products>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import {isEmpty} from 'lodash'
     import {mapState} from 'vuex'
     import ClipLoader from 'vue-spinner/src/ClipLoader'
     import {productApi, reviewApi} from '@/api'
@@ -74,18 +70,15 @@
                 this.loading = true
                 try {
                     const product = await productApi.getProductBySlug(this.$route.params.slug)
+                    // @todo load review later?
+                    const review = await reviewApi.getReviewBySlug(this.$route.params.slug)
                     this.$store.commit('detail/setProduct', product.data)
+                    this.review = review.data.items
                 } catch (error) {
                     this.error = error.toString()
                 }
                 this.loading = false
-                this.fetchReviewDataFromApi()
-            },
-            async fetchReviewDataFromApi () {
-                const review = await reviewApi.getReviewBySlug(this.$route.params.slug)
-                this.review = review.data.items
-            },
-            isEmpty
+            }
         },
         components: {
             ClipLoader,
