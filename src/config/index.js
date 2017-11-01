305f1c44c825e1d73493bc5e0b80329d9392de46
sdleiw <wang@solutiondrive.de>
2017-11-01 13:22:20 +0100

    change default local to en_US

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ export default {
     imageUrl: 'http://sylius-api.dev/media/image/',
     apiUrl: 'http://sylius-api.dev/shop-api',
     apiChannel: 'US_WEB',
-    apiLocale: 'en_GB',
+    apiLocale: 'en_US',
     apiLimit: 6
 }
