8337950da27b10c89528899ca1f4cbbba238025c
sdleiw <wang@solutiondrive.de>
2017-11-01 13:23:33 +0100

    implement vue-tabs-component for detail tab

--- src/views/pages/detail/DetailProductTab.vue
+++ src/views/pages/detail/DetailProductTab.vue
@@ -1,27 +1,53 @@
 <template>
     <div class="tabs is-boxed">
-        <ul>
-            <li class="is-active">
-                <a>
-                    <span>Details</span>
-                </a>
-            </li>
-            <li>
-                <a>
-                    <span>Attributes</span>
-                </a>
-            </li>
-            <li>
-                <a>
-                    <span>Reviews</span>
-                </a>
-            </li>
-        </ul>
+        <tabs :options="{ useUrlFragment: false }" class="container">
+            <tab name="Details">
+                <div class="box tab-box">
+                    <p class="content">
+                        @todo: get details from api
+                    </p>
+                    <p class="content">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias corporis doloremque,
+                        <br>
+                        eveniet harum illum neque nobis nostrum placeat quidem quisquam repellat
+                    </p>
+                </div>
+            </tab>
+            <tab name="Attributes">
+                <div class="box tab-box">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias corporis doloremque, eveniet harum illum neque nobis nostrum placeat quidem quisquam repellat tempora ullam vel velit veniam. A ea fugit provident?
+                </div>
+            </tab>
+            <tab name="Reviews">
+                <div class="box tab-box">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias corporis doloremque, eveniet harum illum neque nobis nostrum placeat quidem quisquam repellat tempora ullam vel velit veniam. A ea fugit provident?
+                </div>
+            </tab>
+        </tabs>
     </div>
 </template>
 
 <script>
+    import {Tabs, Tab} from 'vue-tabs-component'
+
     export default {
-        name: 'detail-product-tab'
+        name: 'detail-product-tab',
+        components: {
+            Tabs,
+            Tab
+        }
     }
 </script>
+
+<style>
+    .tab-box {
+        border-bottom: 1px solid #dbdbdb;
+        border-left: 1px solid #dbdbdb;
+        border-right: 1px solid #dbdbdb;
+        border-radius: 0;
+        -webkit-user-select: text;  /* Chrome all / Safari all */
+        -moz-user-select: text;     /* Firefox all */
+        -ms-user-select: text;      /* IE 10+ */
+        user-select: text;
+    }
+</style>
