0045de1111b9f2dbd2782afbc3c9c96250418b8c
Lei Wang <wang@solutiondrive.de>
2017-11-14 21:14:36 +0100

    add auth login in component

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -6,35 +6,40 @@
         </header>
 
         <section class="box">
-            <form action="">
-                <p><strong>Registered customers</strong></p>
-                <hr class="is-marginless">
-                <p class="content">If you have an account, sign in with your e-mail address.</p>
+            <p><strong>Registered customers</strong></p>
+            <hr class="is-marginless">
+            <p class="content">If you have an account, sign in with your e-mail address.</p>
 
-                <div class="field">
-                    <label class="label">Email</label>
-                    <div class="control">
-                        <input class="input" type="text" value="bulma">
-                    </div>
-                    <!--@todo: validation-->
-                </div>
+            <div v-if="error" class="notification is-danger">{{ error }}</div>
+            <div v-if="validationError" class="notification is-danger">{{ validationError }}</div>
 
-                <div class="field">
-                    <label class="label">Password</label>
-                    <p class="control">
-                        <input class="input" type="password">
-                    </p>
+            <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                    <input class="input" type="text" value="bulma" v-model="email">
                 </div>
+                <!--@todo: validation-->
+            </div>
 
-                <div class="field is-grouped">
-                    <div class="control">
-                        <button class="button is-link">Login</button>
-                    </div>
-                    <div class="control">
-                        <button class="button is-light">@todo: reset password</button>
-                    </div>
+            <div class="field">
+                <label class="label">Password</label>
+                <p class="control">
+                    <input class="input" type="password" v-model="password">
+                </p>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <button @click="login" class="button is-link"
+                            :class="{'is-loading': loading && loadingAction === 'login'}"
+                    >
+                        Login
+                    </button>
                 </div>
-            </form>
+                <div class="control">
+                    <button class="button is-light">@todo: reset password</button>
+                </div>
+            </div>
         </section>
     </div>
 
@@ -42,6 +47,38 @@
 
 <script>
     export default {
-        name: 'login'
+        name: 'login',
+        data () {
+            return {
+                error: '',
+                loading: '',
+                loadingAction: '',
+                validationError: '',
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            async login () {
+                this.validationError = ''
+                if (this.email === '' || this.password === '') {
+                    this.validationError = 'please fill in email and password'
+                    return
+                }
+
+                this.error = ''
+                this.loading = true
+                this.loadingAction = 'login'
+                const loginData = {
+                    email: this.email,
+                    password: this.password
+                }
+                await this.$store.dispatch('auth/login', loginData).catch(error => {
+                    this.error = error.toString()
+                })
+                this.loading = false
+                this.loadingAction = ''
+            }
+        }
     }
 </script>
