c6e54f9862d94373e3cceb528f487bfd7ab4dab8
Lei Wang <wang@solutiondrive.de>
2017-11-14 23:39:43 +0100

    add container for login page

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -1,46 +1,48 @@
 <template>
     <div class="section">
-        <header class="content">
-            <h1 class="title">Customer login</h1>
-            <p class="subtitle">Access your store account or create a new one</p>
-        </header>
+        <div class="container">
+            <header class="content">
+                <h1 class="title">Customer login</h1>
+                <p class="subtitle">Access your store account or create a new one</p>
+            </header>
 
-        <section class="box">
-            <p><strong>Registered customers</strong></p>
-            <hr class="is-marginless">
-            <p class="content">If you have an account, sign in with your e-mail address.</p>
+            <section class="box">
+                <p><strong>Registered customers</strong></p>
+                <hr class="is-marginless">
+                <p class="content">If you have an account, sign in with your e-mail address.</p>
 
-            <div v-if="error" class="notification is-danger">{{ error }}</div>
-            <div v-if="validationError" class="notification is-danger">{{ validationError }}</div>
+                <div v-if="error" class="notification is-danger">{{ error }}</div>
+                <div v-if="validationError" class="notification is-danger">{{ validationError }}</div>
 
-            <div class="field">
-                <label class="label">Email</label>
-                <div class="control">
-                    <input class="input" type="text" value="bulma" v-model="email">
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control">
+                        <input class="input" type="text" value="bulma" v-model="email">
+                    </div>
+                    <!--@todo: validation-->
                 </div>
-                <!--@todo: validation-->
-            </div>
 
-            <div class="field">
-                <label class="label">Password</label>
-                <p class="control">
-                    <input class="input" type="password" v-model="password">
-                </p>
-            </div>
+                <div class="field">
+                    <label class="label">Password</label>
+                    <p class="control">
+                        <input class="input" type="password" v-model="password">
+                    </p>
+                </div>
 
-            <div class="field is-grouped">
-                <div class="control">
-                    <button @click="login" class="button is-link"
-                            :class="{'is-loading': loading && loadingAction === 'login'}"
-                    >
-                        Login
-                    </button>
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button @click="login" class="button is-link"
+                                :class="{'is-loading': loading && loadingAction === 'login'}"
+                        >
+                            Login
+                        </button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-light">@todo: reset password</button>
+                    </div>
                 </div>
-                <div class="control">
-                    <button class="button is-light">@todo: reset password</button>
-                </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </div>
 
 </template>
