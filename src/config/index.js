8d4f9a4b291391904ab7616dc6e4fcc145c235e5
Lei Wang <wang@solutiondrive.de>
2017-11-15 23:13:47 +0100

    update api host

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
     appName: 'Sylius PWA',
-    imageUrl: 'http://sylius-api.dev/media/image/',
-    apiUrl: 'http://sylius-api.dev/shop-api',
+    imageUrl: 'http://syliusapi.dev/media/image/',
+    apiUrl: 'http://syliusapi.dev/shop-api',
     apiChannel: 'US_WEB',
     apiLocale: 'en_US',
     apiLimit: 6
