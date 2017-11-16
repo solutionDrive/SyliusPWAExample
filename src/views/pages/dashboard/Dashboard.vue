ba51a1ed9ca800af833b2fed4d73e3524c199029
Lei Wang <wang@solutiondrive.de>
2017-11-16 21:41:31 +0100

    add dashboard template

--- src/views/pages/dashboard/Dashboard.vue
+++ src/views/pages/dashboard/Dashboard.vue
@@ -1,9 +1,44 @@
 <template>
-
+    <div class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-one-quarter">
+                    <aside class="menu box">
+                        <p class="menu-label">
+                            Your Account
+                        </p>
+                        <ul class="menu-list">
+                            <li v-for="page in accounts">
+                                <router-link :to="{name: 'dashboard'}">
+                                    {{ page.name }}
+                                </router-link>
+                            </li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="column">
+                    <h3 class="title is-3">My account</h3>
+                    <p class="subtitle">Manage your personal information and preferences</p>
+                    <hr class="is-paddingless">
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: 'dashboard'
+        name: 'dashboard',
+        data () {
+            return {
+                accounts: [
+                    {name: 'Dashboard', link: 'dashboard'},
+                    {name: 'Personal information', link: 'personInfo'},
+                    {name: 'Change password', link: 'changePassword'},
+                    {name: 'Address book', link: 'addressBook'},
+                    {name: 'Order history', link: 'orderHistory'}
+                ]
+            }
+        }
     }
 </script>
