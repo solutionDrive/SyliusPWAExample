0d2af9b17a2e37459844dcc40269b8dcca16eaf8
Lei Wang <wang@solutiondrive.de>
2017-11-18 15:05:12 +0100

    finish address form

--- src/views/pages/checkout/Address.vue
+++ src/views/pages/checkout/Address.vue
@@ -23,28 +23,47 @@
                 </div>
 
                 <div class="field">
-                    <label class="label">Email</label>
+                    <label class="label">Street Address</label>
                     <div class="control">
-                        <input class="input" type="email" v-model="email">
+                        <input class="input" type="text" v-model="street">
                     </div>
                 </div>
 
                 <div class="field">
-                    <label class="label">Password</label>
-                    <p class="control">
-                        <input class="input" type="password" v-model="password">
-                    </p>
-                </div>
-
-                <div class="field">
-                    <label class="label">confirmation</label>
-                    <p class="control">
-                        <input class="input" type="password" v-model="confirmation">
-                    </p>
-                </div>
-
-                <div class="field">
+                    <label class="label">City</label>
                     <div class="control">
+                        <input class="input" type="text" v-model="city">
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Postcode</label>
+                    <div class="control">
+                        <input class="input" type="text" v-model="postcode">
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Province name</label>
+                    <div class="control">
+                        <input class="input" type="text" v-model="provinceName">
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Country Code</label>
+                    <div class="control">
+                        <input class="input" type="text" v-model="countryCode">
+                    </div>
+                    <p><small>@todo: need api to get all available Country Code</small></p>
+                </div>
+
+                <div class="field is-grouped">
+                    <div class="control">
+                        <router-link :to="{name: 'home'}" class="button is-light">
+                            back to store
+                        </router-link>
+
                         <button @click="register" class="button is-link"
                                 :class="{'is-loading': loading && loadingAction === 'register'}"
                         >
@@ -59,6 +78,21 @@
 
 <script>
     export default {
-        name: 'checkout-address'
+        name: 'checkout-address',
+        data () {
+            return {
+                firstName: '',
+                lastName: '',
+                countryCode: '',
+                street: '',
+                city: '',
+                postcode: '',
+                provinceName: '',
+                validationError: '',
+                error: '',
+                loading: false,
+                loadingAction: ''
+            }
+        }
     }
 </script>
