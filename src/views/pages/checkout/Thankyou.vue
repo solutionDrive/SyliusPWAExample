24aacf159670b40ba5f6f3795a22f3547d0815a1
Lei Wang <wang@solutiondrive.de>
2017-11-20 22:47:39 +0100

    add thankyou template

--- src/views/pages/checkout/Thankyou.vue
+++ src/views/pages/checkout/Thankyou.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        thankyou
+    <div class="section">
+        <div class="container">
+            <section class="box">
+                <h3 class="title is-3 has-text-centered">thankyou</h3>
+                <p>You have successfully placed an order.</p>
+            </section>
+        </div>
     </div>
 </template>
 
